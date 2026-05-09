9433250f85e0338d22980236f6c2fc14827562ff
davidbro06 <davb060@hotmail.fr>
2026-05-09 18:15:27 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 import express from "express";
 import fetch from "node-fetch";
 const app = express();
-app.use((req,res,next)=>{res.setHeader("Access-Control-Allow-Origin","*");next();});
-app.get("/api/football",async(req,res)=>{
-  try{
-    const{path,...p}=req.query;
-    const url=`https://v3.football.api-sports.io/${path}?${new URLSearchParams(p)}`;
-    const r=await fetch(url,{headers:{"x-apisports-key":"5da71f696f2baa3867a5756bac83a0c0"}});
-    res.json(await r.json());
-  }catch(e){res.status(500).json({error:e.message});}
+
+app.use((req,res,next)=>{
+  res.setHeader("Access-Control-Allow-Origin","*");
+  next();
 });
-app.listen(process.env.PORT||3000,()=>console.log("OK"));
+
+app.get("/api/football", async (req, res) => {
+  try {
+    const { path, ...params } = req.query;
+    const qs = new URLSearchParams(params).toString();
+    const url = `https://api.football-data.org/v4/${path}?${qs}`;
+    const r = await fetch(url, {
+      headers: { "X-Auth-Token": "04c70715613e4222b561202421299a42" }
+    });
+    const data = await r.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+app.listen(process.env.PORT || 3000, () => console.log("OK"));
