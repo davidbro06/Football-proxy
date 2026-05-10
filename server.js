48e7bb61fbf619071a8cfe5104809d47b8349f89
davidbro06 <davb060@hotmail.fr>
2026-05-10 06:48:12 +0200

    Update server.js

--- server.js
+++ server.js
@@ -11,13 +11,19 @@ app.get("/api/football", async (req, res) => {
   try {
     const { path, ...params } = req.query;
     const qs = new URLSearchParams(params).toString();
-    const url = `https://api.football-data.org/v4/${path}?${qs}`;
+    const decodedPath = decodeURIComponent(path);
+    const url = `https://api.football-data.org/v4/${decodedPath}${qs ? "?" + qs : ""}`;
+    console.log("Fetching:", url);
     const r = await fetch(url, {
-      headers: { "X-Auth-Token": "04c70715613e4222b561202421299a42" }
+      headers: { 
+        "X-Auth-Token": "04c70715613e4222b561202421299a42",
+        "Content-Type": "application/json"
+      }
     });
     const data = await r.json();
     res.json(data);
   } catch (err) {
+    console.error(err);
     res.status(500).json({ error: err.message });
   }
 });
